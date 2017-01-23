099888e178f1057e5060a8d4f408dedd4c5938e6
mcnestrb <brianmcnestry@gmail.com>
2017-01-23 14:24:25 +0000

    change routing for admin section -  will need to change this to a subdomain when I get the site hosted properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 	namespace :admin do
-		constraints subdomain: 'admin' do
-			resources :articles, except: [:index, :show]
-			devise_for :admins
-		end
+		resources :articles, except: [:index, :show]
+		devise_for :admins
 	end
 	resources :articles, only: [:index, :show]
 	root 'articles#index'
