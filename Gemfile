d1b96c875d64241fbc280f87140e63549b2ab36c
mcnestrb <brianmcnestry@gmail.com>
2017-02-02 16:33:26 +0000

    add photos to articles

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
+gem "paperclip", "~> 5.0.0"
 group :development, :test do
   gem 'byebug'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118154707) do
+ActiveRecord::Schema.define(version: 20170202154753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,12 @@ ActiveRecord::Schema.define(version: 20170118154707) do
     t.string   "author_name"
     t.integer  "type"
     t.integer  "issue"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
