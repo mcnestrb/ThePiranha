d8291f51062605f13b28500a9a829c7fc0611e67
mcnestrb <brianmcnestry@gmail.com>
2017-05-26 15:13:36 +0100

    style the search bar

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,6 @@ class SearchController < ApplicationController
         if(params[:search])
             @search_term = params[:search]
             @articles = Article.search(params[:search])
-            x = 1
         else
             @search_term = ""
             @articles = Article.none
