31a2c09a56fa1c7c40edcc1e9080728a1f37ead6
mcnestrb <brianmcnestry@gmail.com>
2017-06-13 14:13:21 +0100

    invites work and signing up is fully gone

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
 	resources :articles, :issue_links
 	post '/articles/:id' => 'articles#feature', :as => :feature_article
 	resources :drafts, only: [:edit, :update, :destroy, :index]
-	devise_for :editors, except: [:registrations]
-	devise_for :admins, except: [:registrations]
+	devise_for :editors, :skip => [:registrations]
+	as :editor do
+	  get 'editor/edit' => 'devise/registrations#edit', :as => 'edit_editor_registration'
+	  put 'editor' => 'devise/registrations#update', :as => 'editor_registration'
+	end
+	devise_for :admins, :skip => [:registrations]
+	as :admin do
+	  get 'admin/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+	  put 'admin' => 'devise/registrations#update', :as => 'admin_registration'
+	end
 	match 'admin/:id' => 'admins#destroy', :via => :delete, :as => :admin_destroy_editor
 	get '/admin' => 'admins#home'
 	get '/admin/manage' => 'admins#manage'
