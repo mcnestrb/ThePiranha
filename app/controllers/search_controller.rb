7cbd10fb23c97683f97cbef7cc80e7668041a047
mcnestrb <brianmcnestry@gmail.com>
2017-06-14 20:03:41 +0100

    fix search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,11 @@
 class SearchController < ApplicationController
     def search
-        if(params[:search])
-            @search_term = params[:search]
-            @articles = Article.search(params[:search]).paginate(:page => params[:page], :per_page => 10)
-
-        else
+        if params[:search].blank?
             @search_term = ""
             @articles = Article.none
+        else
+            @search_term = params[:search]
+            @articles = Article.search(params[:search]).paginate(:page => params[:page], :per_page => 10)    
         end
     end
 end
